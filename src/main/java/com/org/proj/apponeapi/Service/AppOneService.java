602ed295ba05b16f898a32b48435a6991b53946f
Anant <anant.saxena08@gmail.com>
2024-04-21 15:12:01 +0530

    Controller and Service layer creation for Filters

---
+++ src/main/java/com/org/proj/apponeapi/Service/AppOneService.java
@@ -0,0 +1,30 @@
+package com.org.proj.apponeapi.Service;
+
+
+
+import java.util.List;
+
+
+public interface AppOneService  {
+
+    List<String> getFirstFilter(String search);
+
+    List<String> getSecondFilter(String search);
+
+    List<String> getThirdFilter(String search);
+
+    List<String> getFourthFilter(String search);
+
+    List<String> getFifthFilter(String search);
+
+    List<String> getSixthFilter(String search);
+
+    List<String> getSeventhFilter(String search);
+
+    List<String> getEighthFilter(String search);
+
+    List<String> getNinthFilter(String search);
+
+    List<String> getTenthFilter(String search);
+
+}
